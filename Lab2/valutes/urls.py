094cf44085e141e1bb5807923622d3322402f3e4
Kronos <arefev.arefev.kirill@mail.ru>
2024-03-11 14:00:15 +0700

    Шаблоны, изменение css

---
+++ Lab2/valutes/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, register_converter, include
+from . import views, converters
+from django.http import HttpResponseNotFound
+
+register_converter(converters.YearConverter, 'year')
+
+urlpatterns = [
+    path('', views.page_from_name, {'file_name': 'index'}, name='index'),
+    path('valute/<str:valute_code>/', views.valute_view, name='valute_page'),
+    path('about_site/', views.page_from_name, {'file_name': 'about_site'}, name='about'),
+    path('about_company/', views.page_from_name, {'file_name': 'about_company'}, name='about_site'),
+    path('faq/', views.page_from_name, {'file_name': 'FAQ'}, name='FAQ'),
+
+    path('color/<str:color>/', views.color_view),
+    path('404/', lambda request: HttpResponseNotFound('Страница не найдена - Ошибка адреса')),
+
+    path('archive/<year:year>/', views.archive_year),
+    path('exchange-rate/<str:valute_code>/', views.get_exchange_rate, name='exchange_rate_form'),
+    path('current-exchange-rate/<str:valute_code>/', views.current_exchange_rate, name='current_exchange_rate'),
+    path('currency-chart/<str:valute_code>/', views.get_currency_rates, name='currency_chart'),
+]
--- Lab2/valutes/valutesBlog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.page_from_name, {'file_name':'valutesBlog/index'}, name='index'),
-]
